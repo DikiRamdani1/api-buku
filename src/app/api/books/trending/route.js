40478ec0e1c535f21b0fde9ae5e5f6e885cfd6cc
DikiRamdani <148926289+DikiRamdani1@users.noreply.github.com>
2024-07-20 20:28:07 +0700

    Update route.js

--- src/app/api/books/trending/route.js
+++ src/app/api/books/trending/route.js
@@ -1,20 +1,62 @@
-import { getDataTrending } from "@/libs/firebase/service";
-import { NextResponse as res } from "next/server";
+import {
+    getDataTrending
+} from "@/libs/firebase/service";
+import {
+    NextResponse as res
+} from "next/server";
 
 export async function GET(request) {
+    const {
+        searchParams
+    } = new URL(request.url)
+    const limit = searchParams.get('limit')
     const token = request.headers.get('authorization')?.split(' ')[1]
 
     if (!token) {
-        return res.json({status: 401, message: "token tidak ada", data: "tidak ada data"})
+        return res.json({
+            status: 401,
+            message: "token tidak ada",
+            data: "tidak ada data"
+        })
     }
 
     if (token != process.env.KEY_API) {
-        return res.json({status: 403, message: "token salah", data: "tidak ada data"})
+        return res.json({
+            status: 403,
+            message: "token salah",
+            data: "tidak ada data"
+        })
     }
     try {
-        const books = await getDataTrending("book")
-        return res.json({status: 200, message: "data behasil ditemukan", data: books})
+        if (limit) {
+            const books = await getDataTrending("book")
+            const book = books.slice(0, limit)
+            if (book.length != 0) {
+                return res.json({
+                    status: 200,
+                    message: "data ditemukan",
+                    data: book
+                })
+            } else {
+                return res.json({
+                    status: 404,
+                    message: "data tidak ditemukan",
+                    data: "data tidak ada"
+                })
+            }
+        } else {
+            const books = await getDataTrending("book")
+            return res.json({
+                status: 200,
+                message: "data behasil ditemukan",
+                data: books
+            })
+        }
     } catch (error) {
-        return res.json({status: 500, message: "data gagal ditemukan", data: "tidak ada data"})
+        return res.json({
+            status: 500,
+            message: "data gagal ditemukan",
+            data: "tidak ada data"
+        })
     }
 }
