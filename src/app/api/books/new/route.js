6179d9eb47f92ffb232f3079b204ec5a887a0e68
DikiRamdani <148926289+DikiRamdani1@users.noreply.github.com>
2024-07-20 20:31:47 +0700

    Update route.js

--- src/app/api/books/new/route.js
+++ src/app/api/books/new/route.js
@@ -2,6 +2,8 @@ import { getDataNew } from "@/libs/firebase/service";
 import { NextResponse as res } from "next/server";
 
 export async function GET(request) {
+    const {searchParams} = new URL(request.url)
+    const limit = searchParams.get("limit")
     const token = request.headers.get('authorization')?.split(' ')[1]
 
     if (!token) {
@@ -12,8 +14,26 @@ export async function GET(request) {
         return res.json({status: 403, message: "token salah", data: "tidak ada data"})
     }
     try {
-        const books = await getDataNew("book")
-        return res.json({status: 200, message: "data behasil ditemukan", data: books})
+        if (limit) {
+            const books = await getDataNew("book")
+            const book = books.slice(0, limit)
+            if (book.length != 0) {
+                return res.json({
+                    status: 200,
+                    message: "data ditemukan",
+                    data: book
+                })
+            } else {
+                return res.json({
+                    status: 404,
+                    message: "data tidak ditemukan",
+                    data: "data tidak ada"
+                })
+            }
+        } else {
+            const books = await getDataNew("book")
+            return res.json({status: 200, message: "data behasil ditemukan", data: books})
+        }
     } catch (error) {
         return res.json({status: 500, message: "data gagal ditemukan", data: "data tidak ada"})
     }
